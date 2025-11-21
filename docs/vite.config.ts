fd6b093d4429cf43d90949ffeb4c2ff8f3da169a
leonardorosasilva <03466316022@edu.udesc.br>
2025-11-20 23:37:15 -0300

    build

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()]  ,
+  base: '/Neomind.github.io/',
   server: {
     proxy: {
       '/api': 'http://localhost:8080'
